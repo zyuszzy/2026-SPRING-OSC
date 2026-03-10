fee5cdad46b840d1a27cfeb5692ae8b7e097cc84
zyuszzy <c27627709@outlook.com>
2026-03-10 21:33:23 +0800

    Update Lab1

--- Lab1/uart.c
+++ Lab1/uart.c
@@ -1,7 +1,13 @@
-#define UART_BASE 0xD4017000UL
-#define UART_RBR  (volatile unsigned char*)(UART_BASE + (0x0 << 2))
-#define UART_THR  (volatile unsigned char*)(UART_BASE + (0x0 << 2))
-#define UART_LSR  (volatile unsigned char*)(UART_BASE + (0x5 << 2))
+#ifdef USE_QEMU
+    #define UART_BASE 0x10000000UL
+    #define UART_STRIDE 1
+#else
+    #define UART_BASE 0xD4017000UL
+    #define UART_STRIDE 4
+#endif
+#define UART_RBR  (volatile unsigned char*)(UART_BASE + (0x0 * UART_STRIDE))
+#define UART_THR  (volatile unsigned char*)(UART_BASE + (0x0 * UART_STRIDE))
+#define UART_LSR  (volatile unsigned char*)(UART_BASE + (0x5 * UART_STRIDE))
 #define LSR_DR    (1 << 0)
 #define LSR_TDRQ  (1 << 5)
 
